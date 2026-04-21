5ab578a76225fcfc63a2c0959e8f96bc7d21f5ab
BlakY <malandealwis@gmail.com>
2026-04-21 22:39:45 +0530

    Initialize Spring Boot project with Maven wrapper, basic structure, and user management API

---
+++ src/main/java/com/example/restAPIdemo/api/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.restAPIdemo.api.controller;
+import com.example.restAPIdemo.api.model.User;
+import com.example.restAPIdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class UserController {
+ private UserService userService;
+ @Autowired
+ public UserController(UserService userService){
+ this.userService=userService;
+ }
+ @GetMapping("/user")
+ public User getUser(@RequestParam Integer id) {
+ User user=userService.getUser(id);
+ return user;
+ }
+}
